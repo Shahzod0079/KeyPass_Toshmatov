a4f23b6b924e38a78f052c4905bebd3d78153808
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-29 20:58:24 +0500

    Разработанные вспомогательные классы для сервера.  Тошматов

---
+++ KeyPass_Toshmatov/Classes/DatabaseManager.cs
@@ -0,0 +1,22 @@
+﻿using KeyPass_Toshmatov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyPass_Toshmatov.Classes
+{
+    public class DatabaseManager : DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseManager () =>
+            Database.EnsureCreated ();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;uid=student;pwd=;database=Storage;",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+
+        }
+    }
+}
