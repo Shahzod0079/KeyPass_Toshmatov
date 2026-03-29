923644a0a09547dbc9603ceb2bacbe2cbba61e66
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-29 18:33:58 +0500

    Разработанные модели для сервера. Тошматов

---
+++ KeyPass_Toshmatov/Models/Storage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KeyPass_Toshmatov.Models
+{
+    public class Storage
+    {
+        [Key]
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? Url { get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+
+        [ForeignKey("UserId")]
+        public User user { get; set; }
+    }
+}
---
+++ KeyPass_Toshmatov/Models/StorageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace KeyPass_Toshmatov.Models
+{
+    public class StorageDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+
+    }
+}
