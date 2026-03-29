9604711eb16290ccbbbcbb08b00bc5ebadc7df98
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-29 18:20:35 +0500

    Подключение Swagger для сервера. Тошматов

--- KeyPass_Toshmatov/Program.cs
+++ KeyPass_Toshmatov/Program.cs
@@ -1,6 +1,30 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Инструментарий"
+    });
+
+    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeePass.xml");
+    options.IncludeXmlComments(PathFile);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Инструментарий");
+});
+
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
