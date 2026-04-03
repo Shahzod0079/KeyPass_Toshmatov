dbfad53be430288fd5bf3493860049c86eae80b9
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-04-03 10:11:14 +0500

    Выполненная работа на оценку Хорошо. Тошматов

--- KeyPass_Toshmatov/Controllers/UserController.cs
+++ KeyPass_Toshmatov/Controllers/UserController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using KeyPass_Toshmatov.Models;
+﻿using System.Security.Cryptography;
+using System.Text;
 using KeyPass_Toshmatov.Classes;
-using System.Linq;
+using KeyPass_Toshmatov.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KeyPass_Toshmatov.Controllers
 {
-    [Route("")]  // ← добавить базовый маршрут
     public class UserController : Controller
     {
         private DatabaseManager databaseManager;
@@ -15,14 +15,25 @@ namespace KeyPass_Toshmatov.Controllers
             this.databaseManager = new DatabaseManager();
         }
 
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(bytes);
+            }
+        }
+
         [Route("login")]
         [HttpPost]
         public ActionResult Login([FromForm] string login, [FromForm] string password)
         {
             try
             {
+                string hashedPassword = HashPassword(password);  
+
                 User? AuthUser = databaseManager.Users
-                    .Where(x => x.Login == login && x.Password == password)
+                    .Where(x => x.Login == login && x.Password == hashedPassword) 
                     .FirstOrDefault();
 
                 if (AuthUser == null)
